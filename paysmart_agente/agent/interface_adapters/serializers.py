cd37c1530956e2764fe9f7a0a622e21c305e6839
bed-com-04-20 <bed-com-04-20@unima.ac.mw>
2025-03-24 11:43:22 -0700

    serializers done

---
+++ paysmart_agente/agent/interface_adapters/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+class ChatMessageSerializer(serializers.Serializer):
+    message = serializers.CharField(
+        max_length=500,
+        required=True,
+        allow_blank=False,
+        help_text="The chat message from the user."
+    )
+
+    def validate_message(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Message must be at least 5 characters long.")
+        return value
+
+class VoiceChatSerializer(serializers.Serializer):
+    audio_file = serializers.FileField()
